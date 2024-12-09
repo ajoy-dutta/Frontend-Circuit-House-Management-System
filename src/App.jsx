0b71c4085f4276c3a2ea0a70c8220decd205df29
Nitay Das <nitaydas910@gmail.com>
2024-12-09 16:58:15 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/home/')
+            .then(response => setMessage(response.data.message))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div className="text-center text-lg">
+            <h1>React + Django Integration</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
