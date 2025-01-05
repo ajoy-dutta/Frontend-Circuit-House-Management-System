32cbed470cabbd66fe1f594091ae9a3bb6c8e50c
Joydip Das <190102.cse@student.just.edu.bd>
2025-01-05 17:06:25 +0600

    changes profile

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,2 +1,2 @@
-// export const baseurl = "http://127.0.0.1:8000/api"
-export const baseurl = "https://chjashore.online/api"
+export const baseurl = "http://127.0.0.1:8000/api"
+// export const baseurl = "https://chjashore.online/api"
