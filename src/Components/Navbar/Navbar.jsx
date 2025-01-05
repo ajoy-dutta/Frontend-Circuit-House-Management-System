fe6b3813992d42f7f8a846977dd924c9dae0e575
Nitay Das <nitaydas910@gmail.com>
2025-01-05 17:46:15 +0600

    changes conference room design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -247,8 +247,8 @@ const Navbar = () => {
               </div>
             </NavLink>
           </div>
-          <div className="nav-start w-3/4">
-            <ul className="hidden md:flex items-center font-normal font-serif px-1 gap-8 whitespace-nowrap">
+          <div className="nav-start w-4/5">
+            <ul className="hidden md:flex lg:flex lg:justify-between items-center font-normal font-serif px-1 gap-5 whitespace-nowrap">
               {navOptions}
             </ul>
           </div>
