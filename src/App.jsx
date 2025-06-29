59975b410b2f941242f37f90726f88adc20f3b98
Nuhin <nusratnuhin18@gmail.com>
2025-06-29 16:06:34 +0600

    thumbnail name corrected name

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,25 @@ function App() {
   return (
     <div className="text-center text-lg">
       {/* Helmet section for meta tags */}
-      <Helmet>
+      {/* <Helmet>
         <title>React + Django Integration</title>
         <meta name="description" content="This is my React-Django integration project." />
 
         {/* Open Graph for social sharing */}
-        <meta property="og:title" content="React + Django Integration" />
+        {/* <meta property="og:title" content="React + Django Integration" />
         <meta property="og:description" content="This is my React-Django integration project." />
         <meta property="og:image" content="https://chjashore.online/thumbnail.png" />
         <meta property="og:url" content="https://chjashore.online/" />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" /> */}
 
         {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="React + Django Integration" />
         <meta name="twitter:description" content="This is my React-Django integration project." />
         <meta name="twitter:image" content="https://chjashore.online/thumbnail.png" />
-      </Helmet>
+      </Helmet> */} */}
 
-      
+
 
       <h1>React + Django Integration</h1>
       <p>{message}</p>
